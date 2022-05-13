acc631b6a14c26ca9af6476f91fd57114033be14
BaekSeungHoon <psh104404@naver.com>
2022-05-13 18:08:10 +0900

    view update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
       <b-button variant="outline-primary">Button</b-button>
     </nav>
