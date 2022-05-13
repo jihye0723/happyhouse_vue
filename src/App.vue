e71529d55d32d58ad263cdcf3c3a33804cd116e5
BaekSeungHoon <psh104404@naver.com>
2022-05-13 17:58:49 +0900

    bootstrap base setting

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,29 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <b-button variant="outline-primary">Button</b-button>
     </nav>
     <router-view />
   </div>
 </template>
+
+<script>
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+export default {
+  name: "App",
+};
+</script>
 
 <style>
 #app {
