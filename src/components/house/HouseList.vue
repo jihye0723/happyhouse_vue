0167fcb718d56af9772ef135e983d140053fa33d
BaekSeungHoon <psh104404@naver.com>
2022-05-13 17:21:45 +0900

    update layout

---
+++ src/components/house/HouseList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HappyhouseVueHouseList',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/InterestView.vue
+++ src/views/InterestView.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'HappyhouseVueHouseRegist',
+  name: 'HappyhouseVueInterestView',
 
   data() {
     return {
