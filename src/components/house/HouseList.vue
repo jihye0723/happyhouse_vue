35c73473cfe660d65f366ea8cf36cc8cdc6927c1
BaekSeungHoon <psh104404@naver.com>
2022-05-13 16:21:07 +0900

    Vue Layout udpate

---
+++ src/components/house/HouseList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VuejsHouseList',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HouseView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VuejsHouseView',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
