67917e37d12c4b88076882e5e299a96386c506bf
choijihye <chlwlgp11@naver.com>
2022-05-14 13:03:18 +0900

    happy

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,14 @@ import ArticleView from "@/views/ArticleView.vue";
 import HouseView from "@/views/HouseView.vue";
 import InterestView from "@/views/InterestView.vue";
 import UserView from "@/views/UserView.vue";
-//import MainContent from "@/components/MainContent.vue";
-import HouseDetail from "@/components/house/HouseDetail.vue";
-import HouseList from "@/components/house/HouseList.vue";
-import HouseRegist from "@/components/house/HouseRegist.vue";
-import UserDelete from "@/components/user/UserDelete.vue";
-import UserLogin from "@/components/user/UserLogin.vue";
-import UserModify from "@/components/user/UserModify.vue";
-import UserRegist from "@/components/user/UserRegist.vue";
+// //import MainContent from "@/components/MainContent.vue";
+// import HouseDetail from "@/components/house/HouseDetail.vue";
+// import HouseList from "@/components/house/HouseList.vue";
+// import HouseRegist from "@/components/house/HouseRegist.vue";
+// import UserDelete from "@/components/user/UserDelete.vue";
+// import UserLogin from "@/components/user/UserLogin.vue";
+// import UserModify from "@/components/user/UserModify.vue";
+// import UserRegist from "@/components/user/UserRegist.vue";
 
 Vue.use(VueRouter);
 
