4c0181a7496b659c6f4082f9e261313c7c3bfd75
choijihye <chlwlgp11@naver.com>
2022-05-18 10:45:28 +0900

    boar

---
+++ src/components/board/BoardList.vue
@@ -0,0 +1,59 @@
+<template>
+  <b-container class="col-8 mt-3">
+    <b-row>
+      <b-col>
+        <b-alert show><h3>글목록</h3></b-alert>
+      </b-col>
+    </b-row>
+    <b-row class="mb-1">
+      <b-col class="text-right">
+        <b-button variant="outline-primary" @click="moveWrite()"
+          >글쓰기</b-button
+        >
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col v-if="articles.length">
+        <b-table-simple hover responsive>
+          <b-thead head-variant="dark">
+            <b-tr>
+              <b-th>글번호</b-th>
+              <b-th>제목</b-th>
+              <b-th>조회수</b-th>
+              <b-th>작성자</b-th>
+              <b-th>작성일</b-th>
+            </b-tr>
+          </b-thead>
+          <tbody>
+            <!-- 하위 component인 ListRow에 데이터 전달(props) -->
+            <board-list-item
+              v-for="article in articles"
+              :key="article.articleno"
+              v-bind="article"
+            />
+          </tbody>
+        </b-table-simple>
+      </b-col>
+      <b-col v-else class="text-center">도서 목록이 없습니다.</b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import http from "@/api/http";
+
+export default {
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  created() {
+    http.get("/board").then(({ data }) => {
+      this.articles = data;
+    });
+  },
+};
+</script>
+
+<style></style>
