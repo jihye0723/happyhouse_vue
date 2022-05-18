91875cae0a265a3effd6c69148096b28a1f49e81
BaekSeungHoon <psh104404@naver.com>
2022-05-18 14:37:26 +0900

    update kakao map api

--- src/components/house/MainKakaoMap.vue
+++ src/components/house/MainKakaoMap.vue
@@ -2,9 +2,6 @@
   <div>
     <div>Kakao map</div>
     <div id="map"></div>
-    <div v-for="(marker, index) of markers" :key="index">
-      {{ marker.code }}
-    </div>
   </div>
 </template>
 
@@ -33,9 +30,9 @@ export default {
       }
     },
     markers: function (value) {
-      this.markerPosition = value;
+      this.markerPositions = value;
       // console.log("markers watch : " + value);
-      this.displayMarker(this.markerPositions);
+      //      this.displayMarker(value[value.length - 1]);
     },
   },
 
@@ -49,42 +46,44 @@ export default {
       document.head.appendChild(script);
     },
     initMap() {
+      this.markerPositions.shift();
+      console.log(this.markerPositions);
+
       var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
       var options = {
         //지도를 생성할 때 필요한 기본 옵션
         center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
         level: 3, //지도의 레벨(확대, 축소 정도)
       };
+      var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+      let isFirst = true;
 
-      this.map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+      this.markerPositions.forEach((element) => {
+        var position = new kakao.maps.LatLng(element.lat, element.lng);
+        var marker = new kakao.maps.Marker({
+          position: position,
+        });
+        marker.setMap(map);
+        if (isFirst) {
+          map.setCenter(position);
+          isFirst = false;
+        }
+      });
     },
 
     displayMarker(markerPositions) {
-      console.log("marker POsitions : " + markerPositions);
-      if (this.markers.length > 0) {
-        this.markers.forEach((marker) => marker.setMap(null));
-      }
-      let positions = [];
-      markerPositions.forEach((element) => {
-        positions.push(new kakao.maps.LatLng(element.lat, element.lng));
-      });
+      console.log("marker Positions : " + markerPositions);
+
+      // if (this.markers.length > 0) {
+      //   this.markers.forEach((marker) => marker.setMap(null));
+      // }
+      // let positions = [];
+      // markerPositions.forEach((element) => {
+      //   positions.push(new kakao.maps.LatLng(element.lat, element.lng));
+      // });
       //   const positions = markerPositions.map(
       //     (position) => new kakao.maps.LatLng(...position)
       //   );
-      if (positions.length > 0) {
-        this.markers = positions.map(
-          (position) =>
-            new kakao.maps.Marker({
-              map: this.map,
-              position,
-            }),
-        );
-        const bounds = positions.reduce(
-          (bounds, latlng) => bounds.extend(latlng),
-          new kakao.maps.LatLngBounds(),
-        );
-        this.map.setBounds(bounds);
-      }
     },
   },
 };
